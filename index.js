f0048a858dabdb1c25f36cbd3d06ec6af3b6f9bf
anyouzy <anyouzy@sina.com>
2019-05-09 00:42:20 +0800

    try extension

--- index.js
+++ index.js
@@ -12,139 +12,18 @@ const {
     DomainAvailable
 } = require('./lib/DomainAvailable');
 
-let sourceDomainList = [
+/* let sourceDomainList = [
     'LuminousLive.be',
     'wallonie-celtique.be',
     'VivaMinerva.be',
-    'av-forever.be',
-    'prive-emmanuelle.be',
-    'xetara.be',
-    'atrconstruct.be',
-    'properelucht.be',
-    'institut-taisnier.be',
-    'geldverdienen-online.be',
-    'LegrandChemin.be',
-    'MarionHansel.be',
-    'cafeschuttershof.be',
-    'CitizenGuard.be',
-    'beeldcomplot.be',
-    'ChickFight.be',
-    'platformcultuurlimburg.be',
-    'VillaCatalpa.be',
-    'vandebontemorgen.be',
-    'Song-Of-Hiawatha.be',
-    'Iron-Gym.be',
-    'gevelwerken-vergelijken.be',
-    'visionbydeloitte.be',
-    'houtmeubel.be',
-    'vds-concepts.be',
-    'jacobz.be',
-    'danklub.be',
-    'wisfaq.be',
-    'Iranians.be',
-    'MagicoWebDesign.be',
-    'academie-info.be',
-    'ZeroToNine.be',
-    'BorderCollieSite.be',
-    'SecTrack.be',
-    'SearchUniversity.be',
-    'ChookDee.be',
-    'Brussels-International.be',
-    'webjournalistiek.be',
-    'FromHouseHerTog.be',
-    'ConventionGamePlay.be',
-    'CoolCoups.be',
-    'TanDartSinHasselt.be',
-    'sint-jacobshuys.be',
-    'sespetitsriens.be',
-    'gay4u.be',
-    'MarathonVanLier.be',
-    'sliksletjes.be',
-    'gvga.be',
-    'PastOpole.be',
-    'MisterBelgiumPersonality.be',
-    'SupersonicFestival.be',
-    'galaccb.be',
-    'DikeOn.be',
-    'TrikeFriends.be',
-    'Studio-Sept.be',
-    'SalonsDumontCarmel.be',
-    'becauseimagirl.be',
-    'jeugdpact.be',
-    'ilestpartitropvite.be',
-    'PizzaGusto.be',
-    'sodirep.be',
-    'fautlefaire.be',
-    'GrainedAmour.be',
-    'RevGeoGate.be',
-    'vrmrapporten.be',
-    'domusbrugensis.be',
-    'SuperHappy.be',
-    'ParCoursVisit.be',
-    'OlympicLanden.be',
-    'CartoonBox.be',
-    'Ashram.be',
-    'mahdia.be',
-    'whoost.be',
-    'ged-archivage.be',
-    'vanhetmanneke.be',
-    'EcoSpanning.be',
-    'vzwcoma.be',
-    'Milo-Events.be',
-    'matsici.be',
-    'happifish.be',
-    'SoBasic.be',
-    'pakaan.be',
-    'NorTom-Computing.be',
-    'bdirectory.be',
-    'NewHaven.be',
-    'House-Of-Harley.be',
-    'paradou.be',
-    'deeltijdsonderwijs-kempen.be',
-    'apdm.be',
-    'OpenSchoolInfo.be',
-    'dienamis.be',
-    'JesuIsFantastic.be',
-    'vzwkiko.be',
-    'Cattery-Alturas.be',
-    'lesartichautsretro.be',
-    'versvandebakker.be',
-    'keuken-renoveren.be',
-    'derdewereldraad.be',
-    'denlavaar.be',
-    'GoldenShower.be',
-    'TheKidsParadise.be',
-    'piknikelektronik.be',
-    'hd-pornofilm.be',
-    'ilgambero.be',
-    'CatteryBeautifulPearl.be',
-    'borderlinepb.be',
-    'leaudelure.be',
-    'dekrieckaert.be',
-    'noordzeetexas.be',
-    'erfgoedcelkortrijk.be',
-    'gusta-antwerpen.be',
-    'verzekeringskantoormol.be',
-    'oterrasses.be',
-    'ilkavermeiren.be',
-    '2014Sochi.be',
-    '123mobielinternet.be',
-    'leonwalry.be',
-    'NivellesCommerces.be',
-    'otoclubje.be',
-    'serdem.be',
-    'vwjl.be',
-    'erfgoedcelterf.be',
-    'frederic-ivanko.be',
-    'TrafficMeter.be',
-    'linksuggestie.be',
-    'brucovo.be',
-    'ThinkFuture.be',
-    'PlanYourEscape.be',
+
+]; */
 
 
-];
+let availableDomainList = [
+    'LuminousLive.be',
 
+]
 let checkList = [];
 
 
@@ -155,13 +34,13 @@ async function main() {
     //获取域名列表----待完成
 
     // 过滤在godday not available的域名
-    let {
+   /*  let {
         availableDomainList,
         availablePrices
     } = await (new DomainAvailable()).check(sourceDomainList);
 
-    if (!availableDomainList.length) return;
-
+    if (!availableDomainList.length) return; */
+    let availablePrices = []
 
     //过滤没有google index或者index大于120的域名
     let {
@@ -170,6 +49,7 @@ async function main() {
         prices
     } = await (new Google()).filterByIndex(availableDomainList, availablePrices);
 
+    return;
     if (!domainList.length) return;
 
     //webarchive过滤
@@ -209,6 +89,5 @@ async function main() {
     (new Mail()).assignTask(checkList);
 
 }
-
 
 main();
